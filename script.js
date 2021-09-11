c510fd74ad7180bc382ede756d2bf24a0983ef96
Ryan <ryanpburnett@yahoo.com>
2021-09-11 09:36:53 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let words = `'Twas brillig, and the slithy toves
+Did gyre and gimble in the wabe:
+All mimsy were the borogoves,
+And the mome raths outgrabe.
+
+“Beware the Jabberwock, my son!
+The jaws that bite, the claws that catch!
+Beware the Jubjub bird, and shun
+The frumious Bandersnatch!”
+
+He took his vorpal sword in hand;
+Long time the manxome foe he sought—
+So rested he by the Tumtum tree
+And stood awhile in thought.
+
+And, as in uffish thought he stood,
+The Jabberwock, with eyes of flame,
+Came whiffling through the tulgey wood,
+And burbled as it came!
+
+One, two! One, two! And through and through
+The vorpal blade went snicker-snack!
+He left it dead, and with its head
+He went galumphing back.
+
+“And hast thou slain the Jabberwock?
+Come to my arms, my beamish boy!
+O frabjous day! Callooh! Callay!”
+He chortled in his joy.
+
+’Twas brillig, and the slithy toves
+Did gyre and gimble in the wabe:
+All mimsy were the borogoves,
+And the mome raths outgrabe.
+`
+
+let wordsArray = words.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()'"]/g, '').toLowerCase().split(' ');
+
+let noAOU = [];
+
+wordsArray.forEach(word => {
+    if (!word.includes('a') && !word.includes('o') && !word.includes('u')) {
+        noAOU.push(word)
+    }
+});
+
+
+
+const counter = (array, value) => {
+    let count = 0;
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            count++
+        }
+    }
+    return count
+};
+
+console.log(counter(noAOU, 'brillig'))
